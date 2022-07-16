7a6e821070ace3180047d7196a2e102fe0604620
Ubuntu <azureuser@TVM.jqyzjlfugfgetirg5sbrw3jrvg.rx.internal.cloudapp.net>
2022-07-16 15:52:25 +0000

    Tarik  django Submission

--- Mini-Projects/comboOffer/comboOffer/settings.py
+++ Mini-Projects/comboOffer/comboOffer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+# python manage.py runserver 0.0.0.0:8000
 from pathlib import Path
 import os
 import environ
