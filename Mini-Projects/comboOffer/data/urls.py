662d5993765fed083e924e13c6afce180aee8054
Gunjot Singh <66637618+9569220756@users.noreply.github.com>
2022-07-18 17:36:57 +0530

    Delete Mini-Projects directory

--- Mini-Projects/comboOffer/data/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.input_doc, name="input_doc"),
-    path('thankyou',views.input_doc,name="input_doc"),
-]
--- Mini-Projects/textConverter/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.input ,name="input"),
-    path('thankyou',views.input,name="input"),
-]
