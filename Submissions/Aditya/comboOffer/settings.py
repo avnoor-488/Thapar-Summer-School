412123f7bdd3ca328cbb40250307a17831530959
tida1732 <79642133+adityaagg7@users.noreply.github.com>
2022-07-19 00:19:04 +0530

    feat: SecretKey, mail
    moved Secret key to .env,
    removed mail credentials,
    DEBUG-> FALSE

--- Submissions/Aditya/comboOffer/settings.py
+++ Submissions/Aditya/comboOffer/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__jze%vaoiw9^pu(=%mtn%ihl439lvuwj-lm_i@0^(y3+jgb%^'
-# SECRET_KEY = env('SECRET_KEY')
+
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
